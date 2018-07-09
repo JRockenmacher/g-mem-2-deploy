ee440867a77de8f33adad7e6a256b7baed597ea5
JRocken <jp.rockenmacher@gmail.com>
2018-07-09 15:16:01 -0600

    changed knexfile to allow for prod env

--- knexfile.js
+++ knexfile.js
@@ -4,39 +4,14 @@ module.exports = {
 
   development: {
     client: 'postgresql',
-    connection: 'postgres:///g-mem-2'
+    connection: process.env.DATABASE_URL || 'postgres:///g-mem-2'
   },
 // only changed the dev object in this one
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+  
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    connection: process.env.DATABASE_URL || 'postgres:///g-mem-2'
   }
 
 };
