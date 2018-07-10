072366a4ab8095ccdcb8b5704bbe050fa146a09a
JRocken <jp.rockenmacher@gmail.com>
2018-07-10 08:52:17 -0600

    pushing finished to github

--- migrations/20180709134101_00_coffees.js
+++ migrations/20180709134101_00_coffees.js
@@ -1,6 +1,6 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTableIfNotExists('coffees', table => {
+  return knex.schema.createTable('coffees', table => {
       table.increments()
       table.text('name')
       table.text('roaster')
